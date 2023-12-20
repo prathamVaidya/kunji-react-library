1817372b97016c86d62a8e91b68a790952533c59
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-12-20 12:55:10 +0530

    Added production config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Change to Production before pushing
-export const DEFAULT_LOGIN_PAGE_URL = "http://localhost:5173"
-export const DEFAULT_AUTHORIZATION_SERVER_URL = "http://localhost:3013"
+export const DEFAULT_LOGIN_PAGE_URL = "https://kunji.prathamvaidya.in"
+export const DEFAULT_AUTHORIZATION_SERVER_URL = "https://api.prathamvaidya.in"
