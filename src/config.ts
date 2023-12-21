421e1c1d00899288a47a3d382aae416e61a7334d
Pratham Vaidya <iamprathamvaidya@gmail.com>
2023-12-21 15:11:26 +0530

    Added local config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
 // Change to Production before pushing
+
+// Local Configuration
+// export const DEFAULT_LOGIN_PAGE_URL = "http://localhost:5173"
+// export const DEFAULT_AUTHORIZATION_SERVER_URL = "http://localhost:3013"
+
 export const DEFAULT_LOGIN_PAGE_URL = "https://kunji.prathamvaidya.in"
 export const DEFAULT_AUTHORIZATION_SERVER_URL = "https://api.prathamvaidya.in"
