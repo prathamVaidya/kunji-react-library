bf00004a3d70ba8db77ce426154665b66fa82894
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-04-08 21:20:26 +0530

    update: using loading state example

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,14 +2,28 @@ import { useEffect } from "react";
 import { useKunji } from "../hooks/useKunji";
 // Todo: Flow to initiate Authentication, handle not logged in state
 function Main() : JSX.Element {
-    const {user, initiateAuthentication, logout, API, oAxios } = useKunji()
+    const {user, loading, initiateAuthentication, logout, API, oAxios } = useKunji()
     useEffect(() => {
         // API.getUserProfile()
         // oAxios.get("http://localhost:3013/test") // token is included in request
             // console.log("Hey")
-    }, [])
+
+            if(loading){
+                console.log("State: Loading")
+            }
+            else{
+                if(user){
+                    console.log("State: LoggedIn")
+                }
+                else{
+                    console.log("State: Not LoggedIn")
+                }
+            }
+            
+    }, [user, loading])
     return (
         <div>
+            {loading && "Authenticating..."}
             {
                 user ? 
                 <div>Hello { user.fullName }
